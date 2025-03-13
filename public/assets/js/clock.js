7ced3044f883c410206010d923b549f002b5b11f
Matteo Sandrin <matteo.sandrin@hotmail.it>
2025-03-13 10:37:57 -0400

    Improve timing logic of clock.js

--- public/assets/js/clock.js
+++ public/assets/js/clock.js
@@ -1,42 +1,52 @@
+let clocks;
+
 window.addEventListener("load", function () {
-    updateAllClocks();
-    this.setInterval(updateAllClocks, 1000);
+  const clock_ids = ["jr-clock", "milano-metro-clock"];
+  clocks = clock_ids.map((id) => ({
+    id,
+    svg: document.getElementById(id).contentDocument,
+    pivot_point: getPivotPoint(id),
+  }));
+  updateAllClocks();
+  this.setInterval(updateAllClocks, 100);
 });
 
 function updateAllClocks() {
-    updateClock("jr-clock");
-    updateClock("milano-metro-clock");
+  clocks.forEach((clock) => updateClock(clock));
 }
 
-function updateClock(clockSVGId) {
-    var svg = document.getElementById(clockSVGId).contentDocument;
+function updateClock(clock) {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const milliseconds = date.getMilliseconds();
 
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+  const hours_hand = clock.svg.getElementById("hours_hand");
+  const minutes_hand = clock.svg.getElementById("minutes_hand");
 
-    const hours_hand = svg.getElementById("hours_hand");
-    const minutes_hand = svg.getElementById("minutes_hand");
+  const minutes_hand_sec = minutes * 60 + seconds + milliseconds / 1000;
+  const hours_hand_sec = (hours % 12) * 60 * 60 + minutes_hand_sec;
 
-    const pivot_point = getPivotPoint(svg);
+  const twelve_hours_sec = 12 * 60 * 60;
+  const hours_rot_angle = (hours_hand_sec / twelve_hours_sec) * 360;
+  hours_hand.setAttribute(
+    "transform",
+    `rotate(${hours_rot_angle} ${clock.pivot_point})`
+  );
 
-    const minutes_hand_sec = minutes * 60 + seconds;
-    const hours_hand_sec = (hours % 12) * 60 * 60 + minutes_hand_sec;
-    
-    const twelve_hours_sec = 12 * 60 * 60;
-    const hours_rot_angle = hours_hand_sec / twelve_hours_sec * 360;
-    hours_hand.setAttribute("transform", "rotate(" + hours_rot_angle + " " + pivot_point +")");
-    
-    const one_hour_sec = 60 * 60;
-    const mins_rot_angle = minutes_hand_sec / one_hour_sec * 360;
-    minutes_hand.setAttribute("transform", "rotate(" + mins_rot_angle + " " + pivot_point +")");
+  const one_hour_sec = 60 * 60;
+  const mins_rot_angle = (minutes_hand_sec / one_hour_sec) * 360;
+  minutes_hand.setAttribute(
+    "transform",
+    `rotate(${mins_rot_angle} ${clock.pivot_point})`
+  );
 }
 
-function getPivotPoint(svg) {
-    const background = svg.getElementById("background");
-    const pivot_x = background.getBBox().width / 2;
-    const pivot_y = background.getBBox().height / 2;
-    const pivot_point = pivot_x.toString() + " " + pivot_y.toString();
-    return pivot_point;
+function getPivotPoint(id) {
+  const svg = document.getElementById(id).contentDocument;
+  const background = svg.getElementById("background");
+  const pivot_x = background.getBBox().width / 2;
+  const pivot_y = background.getBBox().height / 2;
+  return `${pivot_x} ${pivot_y}`;
 }
