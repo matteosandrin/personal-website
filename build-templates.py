70ba108cf4cd3ba9092514671a34b1c054555909
Matteo Sandrin <sandrin98@gmail.com>
2025-11-18 15:25:34 -0500

    Format python

--- build-templates.py
+++ build-templates.py
@@ -6,21 +6,35 @@ import os
 import sys
 
 if len(sys.argv) < 4:
-    print("usage: python build-templates.py <data_filepath> <template_dir> <destination_dir>", file=sys.stderr)
-    print("  <data_filepath>   A JSON file containing data to inject into the templates.", file=sys.stderr)
-    print("  <template_dir>    The directory where the Jinja/HTML templates are located.", file=sys.stderr)
-    print("  <destination_dir> The directory where the built templates will be output to.", file=sys.stderr)
+    print(
+        "usage: python build-templates.py <data_filepath> <template_dir> <destination_dir>",
+        file=sys.stderr,
+    )
+    print(
+        "  <data_filepath>   A JSON file containing data to inject into the templates.",
+        file=sys.stderr,
+    )
+    print(
+        "  <template_dir>    The directory where the Jinja/HTML templates are located.",
+        file=sys.stderr,
+    )
+    print(
+        "  <destination_dir> The directory where the built templates will be output to.",
+        file=sys.stderr,
+    )
     exit(1)
 
 DATA_FILEPATH = os.path.abspath(sys.argv[1])
 TEMPLATE_DIR = os.path.abspath(sys.argv[2])
 DESTINATION_DIR = os.path.abspath(sys.argv[3])
 
+
 def filter_templates(t):
     t_name = os.path.basename(t)
-    is_allowed_extension = (t.endswith(".html") or t.endswith(".txt"))
-    is_partial = (t.startswith("_partials") or t_name.startswith("_"))
+    is_allowed_extension = t.endswith(".html") or t.endswith(".txt")
+    is_partial = t.startswith("_partials") or t_name.startswith("_")
     return is_allowed_extension and (not is_partial)
+
 
 def load_data(data_filepath):
     if not os.path.exists(data_filepath):
@@ -28,6 +42,7 @@ def load_data(data_filepath):
         exit(1)
     data = json.load(open(data_filepath))
     return data
+
 
 def process_single_image(image_url):
     img_partial_path = image_url
@@ -38,10 +53,11 @@ def process_single_image(image_url):
     width, height = im.size
     print(" * ({}, {}) {}".format(width, height, image_url))
     return {
-        "url" : image_url,
-        "width" : width,
-        "height" : height,
+        "url": image_url,
+        "width": width,
+        "height": height,
     }
+
 
 def process_images(data):
     if isinstance(data, dict):
@@ -54,24 +70,30 @@ def process_images(data):
         return process_single_image(data)
     return data
 
+
 def format_iso_date(str_date, format=None):
     date = datetime.datetime.fromisoformat(str_date)
     if format is not None:
         return date.strftime(format)
     return date.isoformat()
 
+
 def cars_sort_date_desc(cars):
-    return sorted(cars, reverse=True, key=lambda c: datetime.datetime.fromisoformat(c["date"]))
+    key = lambda c: datetime.datetime.fromisoformat(c["date"])
+    return sorted(cars, reverse=True, key=key)
+
 
 def cars_sort_star_first(cars):
     # sort by date, descending
     cars = cars_sort_date_desc(cars)
     return [c for c in cars if c["star"]] + [c for c in cars if not c["star"]]
 
+
 def add_custom_filters(env):
-    env.filters['format_iso_date'] = format_iso_date
-    env.filters['cars_sort_star_first'] = cars_sort_star_first
-    env.filters['cars_sort_date_desc'] = cars_sort_date_desc
+    env.filters["format_iso_date"] = format_iso_date
+    env.filters["cars_sort_star_first"] = cars_sort_star_first
+    env.filters["cars_sort_date_desc"] = cars_sort_date_desc
+
 
 data = load_data(DATA_FILEPATH)
 print("Processing images:")
