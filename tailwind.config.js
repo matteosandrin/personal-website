06dc0850a8f9bf24f1fcbb4e67adc96eaf562f77
Matteo Sandrin <sandrin98@gmail.com>
2025-12-27 23:08:33 -0500

    Make styling simpler and more consistent

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
         "blueish-gray": "#F5F9FA",
       },
       fontFamily: {
-        sans: ['"MoMA Sans Web"', ...defaultTheme.fontFamily.sans],
         serif: ['"Perfectly Nineties"', ...defaultTheme.fontFamily.serif],
         mono: ['"Inconsolata"', ...defaultTheme.fontFamily.mono],
       },
